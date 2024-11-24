5e452ab57a138448a2604faefd142b825c494fda
Matsadura <zidanditoo1@gmail.com>
2024-11-24 15:19:53 +0100

    [UPDATE] Remove printf

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: zzaoui <zzaoui@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 11:27:26 by zzaoui            #+#    #+#             */
-/*   Updated: 2024/11/24 14:53:32 by zzaoui           ###   ########.fr       */
+/*   Updated: 2024/11/24 15:19:31 by zzaoui           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,6 +117,5 @@ char	*get_next_line(int fd)
 		free(buffers[fd]);
 		buffers[fd] = NULL;
 	}
-	printf("%d\n", FD_SETSIZE);
 	return (line);
 }
